1701100bc29959aa2109460bf6d40353f20b6f73
KateKolokhina <39592816+KateKolokhina@users.noreply.github.com>
2022-10-10 00:10:43 +0300

    First commit

---
+++ intro-point/src/main/java/com/naukma/intropoint/IntroPointApplication.java
@@ -0,0 +1,13 @@
+package com.naukma.intropoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroPointApplication.class, args);
+	}
+
+}
---
+++ intro-point/src/test/java/com/naukma/intropoint/IntroPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naukma.intropoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
